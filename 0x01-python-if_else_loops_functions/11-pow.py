c24de3be14b26c58ba27abf8854849680e979cf1
netsanet <netsanetbikes@gmail.com>
2022-09-21 05:25:44 -0700

    computes a to the power of b

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def pow(a, b):
+    result = 1
+    if  a == 0:
+        return
+    if b < 0:
+        power = -b
+    else:
+        power = b
+    for i in range(power):
+        result = result * a
+    if b < 0:
+        return 1 / result
+    else:
+        return result
