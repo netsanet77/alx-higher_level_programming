24748183c0d49e139c7f1060a2847e5c214bb572
netsanet <netsanetbikes@gmail.com>
2022-09-22 10:58:21 -0700

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv
-length = len(argv) - 1
-if length == 1:
-    print(f"{length} argument:")
-elif length == 0:
-    print(f"{length} arguments.")
-else:
-    print(f"{length} arguments:")
-for i in range(1, length + 1):
-    print(f"{i}: {argv[i]}")
+if __name__ == '__main__':
+    import sys
+    argv = sys.argv
+    length = len(argv) - 1
+    if length == 1:
+        print(f"{length} argument:")
+    elif length == 0:
+        print(f"{length} arguments.")
+    else:
+        print(f"{length} arguments:")
+    for i in range(1, length + 1):
+        print(f"{i}: {argv[i]}")
