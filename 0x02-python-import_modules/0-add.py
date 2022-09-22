c8529d75085efe4fa6462fa9054666e0ed59988f
netsanet <netsanetbikes@gmail.com>
2022-09-22 06:45:34 -0700

    correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
 if __name__ != '__import__':
+    from add_0 import add
+    a = 1
+    b = 2
     s = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, s))
+    print("{:d} + {:d} = {:d}".format(a, b, s))
