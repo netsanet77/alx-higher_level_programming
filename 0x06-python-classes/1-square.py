65afdb92bce52a053f3ce23528b233f5e8184ac2
netsanet <netsanetbikes@gmail.com>
2022-10-11 05:10:30 -0700

    correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Define square class"""
+
+
 class Square:
+    """Representing a square"""
+
     def __init__(self, size):
+        """Initializing a new square
+
+        Args:
+            size (int): the size of the new square
+        """
         self.__size = size
