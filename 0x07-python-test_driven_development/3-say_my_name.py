24c9bb2e1bd81ec5ddffd521534df2d499d04fc4
netsanet <netsanetbikes@gmail.com>
2022-10-21 14:06:22 -0700

    commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    """print My name is <first name> <last name>
+    Args:
+        first_name (string): first name
+        last_name (string): last name
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
