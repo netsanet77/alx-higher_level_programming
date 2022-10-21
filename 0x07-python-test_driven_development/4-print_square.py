9346204c81601038188a15120d12da467c9c0e04
netsanet <netsanetbikes@gmail.com>
2022-10-21 14:20:36 -0700

    commit

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_square(size):
+    """print square with the character #
+    Args:
+        size (int): the size of the square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(0, size):
+        for j in range(0, size):
+            print("#", end="")
+        print()
