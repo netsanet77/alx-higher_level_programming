324832eb31b523bb161687edfa1c10d7e7cb5244
netsanet <netsanetbikes@gmail.com>
2022-10-24 09:02:50 -0700

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class of BaseGeomety"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
