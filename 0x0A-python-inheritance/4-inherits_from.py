aad76b6329b96168a0177987a2059bc9990b8920
netsanet <netsanetbikes@gmail.com>
2022-10-24 08:43:06 -0700

    correction

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,6 @@ def inherits_from(obj, a_class):
     If the object is an instance of a class that inherited (directly or
     indirectly) from the specified class, return True other wise return False
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return False
-    elif isinstance(obj, a_class):
-        return True
+    return True
