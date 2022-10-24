98751b22a6619b6bd85c8214ec5c55e2578b3a3f
netsanet <netsanetbikes@gmail.com>
2022-10-24 08:38:02 -0700

    only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Inherits from function"""
+
+
+def inherits_from(obj, a_class):
+    """
+    If the object is an instance of a class that inherited (directly or
+    indirectly) from the specified class, return True other wise return False
+    """
+    if type(obj) == a_class:
+        return False
+    return True
