785d09880d894afadcc2514a6f4ddf22f22742eb
netsanet <netsanetbikes@gmail.com>
2022-10-24 11:38:07 -0700

    correction

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,6 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Square class representation"""
     def __init__(self, size):
         """Initializing new square"""
         self.integer_validator("size", size)
