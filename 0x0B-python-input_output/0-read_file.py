6624c72d284670fc4ca9311be00ae623c1771799
netsanet <netsanetbikes@gmail.com>
2022-10-25 11:08:13 -0700

    commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """Reads a text file and print it to stdout"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename) as f:
         r = f.read()
         print(r, end="")
+    f.close
