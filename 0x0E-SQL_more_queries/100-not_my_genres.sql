b2ca33f90d07f1beb8c98f8152d9c296f08f1af7
netsanet <netsanetbikes@gmail.com>
2022-12-01 15:57:53 -0800

    commit

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -12,6 +12,6 @@ WHERE tv_genres.id NOT IN
 	INNER JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.genre_id
 	INNER JOIN tv_show
-	ON tv_show.id = tv_show_genres.show_id
+	ON tv_shows.id = tv_show_genres.show_id
 	WHERE tv_show.title "Dexter")
 ORDER BY tv_genres.name
