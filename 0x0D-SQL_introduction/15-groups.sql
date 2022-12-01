f1c4a96bd7c577881a6feb08877d8cc2f02f2830
netsanet <netsanetbikes@gmail.com>
2022-12-01 05:20:05 -0800

    number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- Lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+-- display the score and number of record for this score as number
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
