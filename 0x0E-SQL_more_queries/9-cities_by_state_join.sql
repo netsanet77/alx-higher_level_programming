cdd189d0710288140a5795492844b221154944eb
netsanet <netsanetbikes@gmail.com>
2022-12-01 13:22:54 -0800

    commit

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,6 @@
 -- Each record should display cities.id, cities.name and states.name
 -- Results must be sorted in ascending order by cities.id
 SELECT cities.id, cities.name, states.name 
-FROM cities,states
-WHERE cities.states_id = states.id 
+FROM cities, states
+WHERE cities.state_id = states.id 
 ORDER BY cities.id;
