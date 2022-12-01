cf67d8785b415e3c5ee02cc83045345ec27b3219
netsanet <netsanetbikes@gmail.com>
2022-12-01 13:20:48 -0800

    commit

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,7 @@
 -- lists all cities contained in the database hbtn_0d_usa
 -- Each record should display cities.id, cities.name and states.name
 -- Results must be sorted in ascending order by cities.id
-SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.id = states.id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name 
+FROM cities,states
+WHERE cities.states_id = states.id 
+ORDER BY cities.id;
