b0154aa3b21721717aea31aeafba777d29076ded
netsanet <netsanetbikes@gmail.com>
2022-12-01 16:43:42 -0800

    commit

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -7,9 +7,7 @@ SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-INNER JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_show_ratings
-ON tv_shows.id = tv_show_ratings.show_id
+ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.id
-ORDER BY rating
+ORDER BY rating DESC;
