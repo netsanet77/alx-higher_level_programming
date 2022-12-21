f99a5e2efbbb3db230840c09fa86518d8169e3a9
netsanet <netsanetbikes@gmail.com>
2022-12-20 16:00:09 -0800

    correction

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        process.stdout.write('#');
+        process.stdout.write('X');
       }
       console.log();
     }
