a600c580b06f1baccd1463132fd521e5b0aa51c4
netsanet <netsanetbikes@gmail.com>
2022-12-21 13:37:30 -0800

    commit

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const a = parseInt(process.argv[2]);
+
+function factorial (x) {
+  if (x === 0) {
+    return (1);
+  } else if (x < 0) {
+    return (0);
+  } else {
+    return (x * factorial(x - 1));
+  }
+}
+
+if (isNaN(a)) {
+  console.log(1);
+} else {
+  console.log(factorial(a));
+}
