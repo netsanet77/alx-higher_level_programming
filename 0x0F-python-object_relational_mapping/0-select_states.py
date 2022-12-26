2934afb95b30081f69bf4f14d600fbe2cd8d70a2
netsanet <netsanetbikes@gmail.com>
2022-12-26 06:56:28 -0800

    commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,10 +6,11 @@ A script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-datab = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                        db=argv[3], port=3306)
-c = datab.cursor()
-c.execute("""SELECT * FROM states""")
-rows = c.fetchall()
-for row in rows:
-    print(row)
+if __name__ == "__main__":
+    datab = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                            db=argv[3], port=3306)
+    c = datab.cursor()
+    c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
