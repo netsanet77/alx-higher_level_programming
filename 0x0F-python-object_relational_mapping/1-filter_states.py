17bf1e2a929907f99212d7e7875a0e7cf496953f
netsanet <netsanetbikes@gmail.com>
2022-12-26 07:35:33 -0800

    commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    datab = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                            db=argv[3], port=3306)
+    c = datab.cursor()
+    c.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+              ORDER BY states.id ASC""")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
