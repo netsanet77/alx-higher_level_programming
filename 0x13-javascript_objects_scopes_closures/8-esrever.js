b402b176e01a88eae3fc4c9c8bbf94bc40808fa3
netsanet77 <netsanetbikes@gmail.com>
2023-03-18 07:41:35 -0700

    commit

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let i = 0;
+  let j = 0;
+  const temp = [];
+  while (i < list.length) {
+    i++;
+  }
+  while (i >= 0) {
+    temp[j] = list[i];
+    i -= 1;
+    j += 1;
+  }
+  return temp;
+};
