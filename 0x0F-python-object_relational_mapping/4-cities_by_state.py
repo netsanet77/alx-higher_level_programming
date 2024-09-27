5287c8909cc0655ab6ac30113116e45acd25a64e
netsanet77 <106907493+netsanet77@users.noreply.github.com>
2024-09-27 23:42:20 +0300

    commit

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    datab = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                            db=argv[3], port=3306)
+    c = datab.cursor()
+    c.execute("""SELECT cities.id, cities.name, states.name \
+            FROM cities, states \
+            WHERE cities.state_id = states.id \
+            ORDER BY cities.id ASC""")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    datab.close()
