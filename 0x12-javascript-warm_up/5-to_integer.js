b14d912403f384e588756a4abd6c43b408fee291
netsanet77 <netsanetbikes@gmail.com>
2024-09-27 06:50:58 -0700

    commit

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2]);
+
 if (Number.isSafeInteger(num)) {
-	  console.log('My number: ' + num);
+  console.log('My number: ' + num);
 } else {
-	  console.log('Not a number');
+  console.log('Not a number');
 }
