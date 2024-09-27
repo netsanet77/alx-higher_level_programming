ca965898a37a372cb71c79bbed0b2204cdbdb54e
netsanet77 <106907493+netsanet77@users.noreply.github.com>
2024-09-27 17:20:58 +0300

    commit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,10 +4,10 @@ const size = parseInt(process.argv[2]);
 if (Number.isSafeInteger(size)) {
   let str = 'X';
   for (let i = 1; i < size; i++) {
-      str += 'X';
+    str += 'X';
   }
   for (let j = 0; j < size; j++) {
-      console.log(str);
+    console.log(str);
   }
 } else {
   console.log('Missing size');
